33d61452aff3dd1c2f05d2efd874751eed65e693
Susmita-Dey <susmitadey475@gmail.com>
2024-12-07 13:30:39 +0530

    add justify-between

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -22,7 +22,7 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
   };
 
   return (
-    <div className="flex justify-between items-center p-2 border-b">
+    <div>
       <motion.div
         layout
         initial={{ opacity: 0, y: 10 }}
@@ -30,44 +30,46 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
         exit={{ opacity: 0, y: -10 }}
         transition={{ duration: 0.3 }}
       >
-        {isEditing ? (
-          <input
-            type="text"
-            value={editText}
-            onChange={(e) => setEditText(e.target.value)}
-            className="flex-1 p-1 border rounded mr-2"
-          />
-        ) : (
-          <span
-            onClick={() => toggleTodo(id)}
-            className={`cursor-pointer flex-1 ${
-              isCompleted ? "line-through" : ""
-            }`}
-          >
-            {text}
-          </span>
-        )}
-        {isEditing ? (
+        <div className="flex justify-evenly items-stretch p-2 border-b border-t">
+          {isEditing ? (
+            <input
+              type="text"
+              value={editText}
+              onChange={(e) => setEditText(e.target.value)}
+              className="flex-1 p-1 border rounded mr-2"
+            />
+          ) : (
+            <span
+              onClick={() => toggleTodo(id)}
+              className={`cursor-pointer flex-1 ${
+                isCompleted ? "line-through" : ""
+              }`}
+            >
+              {text}
+            </span>
+          )}
+          {isEditing ? (
+            <button
+              onClick={handleSave}
+              className="p-1 bg-green-500 text-white rounded hover:bg-green-600 mr-2"
+            >
+              Save
+            </button>
+          ) : (
+            <button
+              onClick={() => setIsEditing(true)}
+              className="p-1 bg-yellow-500 text-white rounded hover:bg-yellow-600 mr-2"
+            >
+              Edit
+            </button>
+          )}
           <button
-            onClick={handleSave}
-            className="p-1 bg-green-500 text-white rounded hover:bg-green-600 mr-2"
+            onClick={() => deleteTodo(id)}
+            className="text-red-500 hover:underline"
           >
-            Save
+            Delete
           </button>
-        ) : (
-          <button
-            onClick={() => setIsEditing(true)}
-            className="p-1 bg-yellow-500 text-white rounded hover:bg-yellow-600 mr-2"
-          >
-            Edit
-          </button>
-        )}
-        <button
-          onClick={() => deleteTodo(id)}
-          className="text-red-500 hover:underline"
-        >
-          Delete
-        </button>
+        </div>
       </motion.div>
     </div>
   );
